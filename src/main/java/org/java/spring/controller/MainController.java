71b9384142a771f1699dc15506f0c49c0a96c1a6
Tommicavo <tommicava@icloud.com>
2023-12-04 14:54:38 +0100

    createPizza

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -5,9 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.java.spring.db.serv.PizzaService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 import java.util.List;
 import org.java.spring.db.pojo.Pizza;
@@ -39,5 +44,30 @@ public class MainController {
 		Pizza pizza = pizzaService.findById(id);
 		model.addAttribute("pizza", pizza);
 		return "pizza";
+	}
+
+	@GetMapping("/pizzas/create")
+	public String create(Model model) {
+
+		Pizza pizza = new Pizza();
+		model.addAttribute(pizza);
+		return "createPizza";
+	}
+
+	@PostMapping("/pizzas/create")
+	public String store(
+			Model model,
+			@Valid @ModelAttribute Pizza pizza,
+			BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+
+			System.out.println(bindingResult);
+			model.addAttribute("pizza", pizza);
+			return "createPizza";
+		}
+
+		pizzaService.save(pizza);
+		return "redirect:/";
 	}
 }
